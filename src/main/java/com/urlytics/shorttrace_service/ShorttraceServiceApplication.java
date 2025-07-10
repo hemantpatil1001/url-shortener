494354413fe7c288ea4af71fcf8be9255788cfd9
Hemant Patil <hemantpatil1001@gmail.com>
2025-07-10 23:52:16 +0530

    implemented hashing algorithm and shortened a sample url

--- src/main/java/com/urlytics/shorttrace_service/ShorttraceServiceApplication.java
+++ src/main/java/com/urlytics/shorttrace_service/ShorttraceServiceApplication.java
@@ -1,7 +1,6 @@
 package com.urlytics.shorttrace_service;
 
 import com.urlytics.shorttrace_service.service.UrlShortener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
